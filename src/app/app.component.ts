5d3d4b8e413ff8356498ae4ee7eac27c406faa6b
Pranav Bhatia <bhatiapranav1996@gmail.com>
2022-10-12 12:08:15 -0400

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay, switchMap, tap } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
-import { AddressModel } from './address.model';
 import { BusinessInfo } from './business-info-model';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
+import { ReadCsvService } from './read-csv.service';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +13,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   language = 'en';
-
-  public addresses: AddressModel[] = [];
   public companies: BusinessInfo[] = [];
 
   isHandset$: Observable<boolean> = this.breakpointObserver
@@ -26,82 +23,18 @@ export class AppComponent implements OnInit {
     );
 
   constructor(
-    private breakpointObserver: BreakpointObserver,
-    private httpClient: HttpClient,
-    private activatedRoute: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private readCsvService: ReadCsvService,
+    private breakpointObserver: BreakpointObserver
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.queryParamMap.subscribe((params) => {
-      var testValue = params.get('locale');
-      console.log(testValue);
-      console.log(params.get('locale'));
-    });
-    this.httpClient
-      .get('assets/desta.csv', { responseType: 'text' })
-      .subscribe((data) => {
-        let csvToRowArray = data.split('\n');
-
-        let headers = csvToRowArray[0].trim().split(', ');
-        console.log(headers);
-
-        csvToRowArray.forEach((row, index) => {
-          if (index === 0) {
-            return;
-          }
-          const businessInfoArray = row.trim().split('|');
-          this.companies.push(
-            new BusinessInfo(
-              businessInfoArray[0],
-              businessInfoArray[1],
-              businessInfoArray[2],
-              businessInfoArray[3],
-              businessInfoArray[4],
-              businessInfoArray[5],
-              businessInfoArray[6],
-              businessInfoArray[7],
-              businessInfoArray[8],
-              businessInfoArray[9],
-              businessInfoArray[10],
-              businessInfoArray[11],
-              businessInfoArray[12],
-              businessInfoArray[13],
-              businessInfoArray[14]
-            )
-          );
-        });
-
-        console.log(this.companies);
+    this.companies = this.readCsvService.companies;
+    if (!this.companies?.length) {
+      this.readCsvService.fetchDataFromCsv().subscribe(() => {
+        this.companies = this.readCsvService.companies;
       });
-
-    this.httpClient
-      .get('assets/addresses.csv', { responseType: 'text' })
-      .subscribe((data) => {
-        let csvToRowArray = data.split('\n');
-
-        let headers = csvToRowArray[0].trim().split(', ');
-        console.log(headers);
-
-        csvToRowArray.forEach((row, index) => {
-          if (index === 0) {
-            return;
-          }
-          const addressArray = row.trim().split(',');
-          this.addresses.push(
-            new AddressModel(
-              addressArray[0],
-              addressArray[1],
-              addressArray[2],
-              addressArray[3],
-              addressArray[4],
-              addressArray[5]
-            )
-          );
-        });
-
-        console.log(this.addresses);
-      });
+    }
   }
 
   public changeLanguage() {
